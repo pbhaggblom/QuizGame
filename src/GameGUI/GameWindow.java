e5c3517bd1a9617ccfb9b759d6b37c563680565f
Sandra Eng <sandra.m.b.eng@hotmail.com>
2024-11-21 15:44:53 +0100

    GameWindow with classes for all panels

--- src/GameGUI/GameWindow.java
+++ src/GameGUI/GameWindow.java
@@ -11,16 +11,16 @@ public class GameWindow extends JFrame {
     JButton startButton = new JButton();
     JButton category1 = new JButton();
     JButton category2 = new JButton();
-    JButton question1 = new JButton();
-    JButton question2 = new JButton();
-    JButton question3 = new JButton();
-    JButton question4 = new JButton();
+    JButton answer1 = new JButton();
+    JButton answer2 = new JButton();
+    JButton answer3 = new JButton();
+    JButton answer4 = new JButton();
+
     public GameWindow() {
         setTitle("Quiz Game");
-        add(panelStart);
         panelStart.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        startWindow(c);
+
+        startWindow();
 
         setSize(400, 700);
         setVisible(true);
@@ -28,61 +28,60 @@ public class GameWindow extends JFrame {
         setLocationRelativeTo(null);
 
     }
-    public void startWindow(GridBagConstraints c) {
-        c.anchor = GridBagConstraints.PAGE_START;
-        c.gridx = 0;
-        c.gridy = 0;
-        panelStart.add(label, c);
 
-        changePanelContent(panelStart, startButton, 0 ,2, "Start game");
-    }
-    public void categoryWindow() {
-        changePanel(panelCategories);
+    public void startWindow() {
+        StartWindow startWindow = new StartWindow(panelStart, startButton, 0, 1,
+                "Starta nytt spel", label);
+        startWindow.changePanel(panelStart);
+        panelStart = startWindow.changePanelContent();
+        add(panelStart);
 
-        changePanelContent(panelCategories, category1, 0, 0, "Category 1");
-
-        changePanelContent(panelCategories, category2, 1, 0, "Category 2");
-
-    }
-    public void questionsWindow() {
-        changePanel(panelQuestions);
-
-        changePanelContent(panelQuestions, question1, 0, 0, "Question 1");
-
-        changePanelContent(panelQuestions, question2, 1, 0, "Question 2");
-
-        changePanelContent(panelQuestions, question3, 0, 1, "Question 3");
-
-        changePanelContent(panelQuestions, question4, 1, 1, "Question 4");
-
-    }
-    public void changePanel(JPanel panel) {
-        add(panel);
-        panel.setLayout(new GridBagLayout());
-    }
-    public void changePanelContent(JPanel panel, JButton button, int x, int y, String text) {
-        GridBagConstraints c = new GridBagConstraints();
-        button.setPreferredSize(new Dimension(150, 70));
-        button.setSize(100, 40);
-        button.setText(text);
-        c.gridx = x;
-        c.gridy = y;
-        c.insets = new Insets(10, 10, 10, 10);
-
-        panel.add(button, c);
-        button.addActionListener(e -> {
-            if(panel.equals(panelStart)) {
-                panel.setVisible(false);
-                categoryWindow();
-            }else if (panel.equals(panelCategories)) {
-                panel.setVisible(false);
-                questionsWindow();
-            }
+        startButton.addActionListener(e -> {
+            panelStart.setVisible(false);
+            categoryWindow();
         });
+    }
+
+    public void categoryWindow() {
+        CategoryWindow categoryWindow = new CategoryWindow(panelCategories, category1,
+                category2, 0, 0, "Kategori 1", "Kategori 2");
+        categoryWindow.changePanel(panelCategories);
+        panelCategories = categoryWindow.changePanelContent();
+        add(panelCategories);
+
+        category1.addActionListener(e -> {
+            panelCategories.setVisible(false);
+            questionsWindow();
+        });
+        category2.addActionListener(e -> {
+            panelCategories.setVisible(false);
+            questionsWindow();
+        });
+    }
+
+    public void questionsWindow() {
+        QuestionsWindow questionsWindow = new QuestionsWindow(panelQuestions, answer1,
+                answer2, answer3, answer4, 0, 0,
+                "Svar 1", "Svar 2", "Svar 3", "Svar 4");
+        questionsWindow.changePanel(panelQuestions);
+        panelQuestions = questionsWindow.changePanelContent();
+        add(panelQuestions);
+
+        answer1.addActionListener(e -> {
+            System.out.println("Du valde svar 1");
+        });
+        answer2.addActionListener(e -> {
+            System.out.println("Du valde svar 2");
+        });
+        answer3.addActionListener(e -> {
+            System.out.println("Du valde svar 3");
+        });
+        answer4.addActionListener(e -> {
+            System.out.println("Du valde svar 4");
+        });
+
     }
     public static void main(String[] args) {
         new GameWindow();
     }
 }
-
-
