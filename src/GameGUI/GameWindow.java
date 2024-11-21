9dffd4a3f3cdfa8604e9dc8d0e8b5ad0ce04e48e
Sandra Eng <sandra.m.b.eng@hotmail.com>
2024-11-21 11:39:52 +0100

    Buttontext is set in method

--- src/GameGUI/GameWindow.java
+++ src/GameGUI/GameWindow.java
@@ -8,13 +8,13 @@ public class GameWindow extends JFrame {
     JPanel panelCategories = new JPanel();
     JPanel panelQuestions = new JPanel();
     JLabel label = new JLabel("Quiz Game");
-    JButton startButton = new JButton("Starta nytt spel");
-    JButton category1 = new JButton("Category 1");
-    JButton category2 = new JButton("Category 2");
-    JButton question1 = new JButton("Question 1");
-    JButton question2 = new JButton("Question 2");
-    JButton question3 = new JButton("Question 3");
-    JButton question4 = new JButton("Question 4");
+    JButton startButton = new JButton();
+    JButton category1 = new JButton();
+    JButton category2 = new JButton();
+    JButton question1 = new JButton();
+    JButton question2 = new JButton();
+    JButton question3 = new JButton();
+    JButton question4 = new JButton();
     public GameWindow() {
         setTitle("Quiz Game");
         add(panelStart);
@@ -22,7 +22,7 @@ public class GameWindow extends JFrame {
         GridBagConstraints c = new GridBagConstraints();
         startWindow(c);
 
-        setSize(400, 800);
+        setSize(400, 700);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -34,38 +34,41 @@ public class GameWindow extends JFrame {
         c.gridy = 0;
         panelStart.add(label, c);
 
-        changePanelContent(panelStart, startButton, 0 ,2);
+        changePanelContent(panelStart, startButton, 0 ,2, "Start game");
     }
     public void categoryWindow() {
         changePanel(panelCategories);
 
-        changePanelContent(panelCategories, category1, 0, 0);
+        changePanelContent(panelCategories, category1, 0, 0, "Category 1");
 
-        changePanelContent(panelCategories, category2, 0, 1);
+        changePanelContent(panelCategories, category2, 1, 0, "Category 2");
 
     }
     public void questionsWindow() {
         changePanel(panelQuestions);
 
-        changePanelContent(panelQuestions, question1, 0, 0);
+        changePanelContent(panelQuestions, question1, 0, 0, "Question 1");
 
-        changePanelContent(panelQuestions, question2, 0, 1);
+        changePanelContent(panelQuestions, question2, 1, 0, "Question 2");
 
-        changePanelContent(panelQuestions, question3, 0, 2);
+        changePanelContent(panelQuestions, question3, 0, 1, "Question 3");
 
-        changePanelContent(panelQuestions, question4, 0, 3);
+        changePanelContent(panelQuestions, question4, 1, 1, "Question 4");
 
     }
     public void changePanel(JPanel panel) {
         add(panel);
         panel.setLayout(new GridBagLayout());
     }
-    public void changePanelContent(JPanel panel, JButton button, int x, int y) {
+    public void changePanelContent(JPanel panel, JButton button, int x, int y, String text) {
         GridBagConstraints c = new GridBagConstraints();
-        button.setPreferredSize(new Dimension(150, 40));
+        button.setPreferredSize(new Dimension(150, 70));
         button.setSize(100, 40);
+        button.setText(text);
         c.gridx = x;
         c.gridy = y;
+        c.insets = new Insets(10, 10, 10, 10);
+
         panel.add(button, c);
         button.addActionListener(e -> {
             if(panel.equals(panelStart)) {
